653d1ee00aa494c491efaed27c51eb5a74d0548c
Axel Donath <axel.donath@cfa.harvard.edu>
2025-03-20 15:36:18 -0400

    Update pre-commit config

--- plot.py
+++ plot.py
@@ -4,33 +4,38 @@ from matplotlib import collections as mc
 from matplotlib import colors as mcolors
 
 
-def plot_intro_diagram(model):
-
-    cfg = model.config
+def plot_intro_diagram(model, config):
+    """Plot intro diagram"""
     WA = model.W.detach()
-    N = len(WA[:,0])
-    sel = range(config.n_instances) # can be used to highlight specific sparsity levels
-    plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.viridis(model.importance[0].cpu().numpy()))
-    plt.rcParams['figure.dpi'] = 200
-    fig, axs = plt.subplots(1,len(sel), figsize=(2*len(sel),2))
-    
+    sel = range(config.n_instances)  # can be used to highlight specific sparsity levels
+    plt.rcParams["axes.prop_cycle"] = plt.cycler(
+        "color", plt.cm.viridis(model.importance[0].cpu().numpy())
+    )
+    plt.rcParams["figure.dpi"] = 200
+    fig, axs = plt.subplots(1, len(sel), figsize=(2 * len(sel), 2))
+
     for i, ax in zip(sel, axs):
         W = WA[i].cpu().detach().numpy()
-        colors = [mcolors.to_rgba(c)
-            for c in plt.rcParams['axes.prop_cycle'].by_key()['color']]
-        ax.scatter(W[:,0], W[:,1], c=colors[0:len(W[:,0])])
-        ax.set_aspect('equal')
-        ax.add_collection(mc.LineCollection(np.stack((np.zeros_like(W),W), axis=1), colors=colors))
-        
+        colors = [
+            mcolors.to_rgba(c)
+            for c in plt.rcParams["axes.prop_cycle"].by_key()["color"]
+        ]
+        ax.scatter(W[:, 0], W[:, 1], c=colors[0 : len(W[:, 0])])
+        ax.set_aspect("equal")
+        ax.add_collection(
+            mc.LineCollection(np.stack((np.zeros_like(W), W), axis=1), colors=colors)
+        )
+
         z = 1.5
-        ax.set_facecolor('#FCFBF8')
+        ax.set_facecolor("#FCFBF8")
         ax.set_xlim((-z, z))
         ax.set_ylim((-z, z))
-        ax.tick_params(left = True, right = False , labelleft = False ,
-                    labelbottom = False, bottom = True)
-        
-        for spine in ['top', 'right']:
+        ax.tick_params(
+            left=True, right=False, labelleft=False, labelbottom=False, bottom=True
+        )
+
+        for spine in ["top", "right"]:
             ax.spines[spine].set_visible(False)
-        
-        for spine in ['bottom','left']:
-            ax.spines[spine].set_position('center')
+
+        for spine in ["bottom", "left"]:
+            ax.spines[spine].set_position("center")
